d8d133751652bed69a0e540dc8e10bf8dca5cc1e
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-28 14:20:19 -0700

    :art: Tweaks to source root

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 'use babel'
 
 import {transform} from 'babel-core'
+import Path from 'path'
 
 export const compiler = true
 export const minifier = false
-export function process(contents, {fileName, relativePath, root}, {config, state}) {
+export function process(contents, {fileName, relativePath}, {config, state}) {
   const beginning = contents.substr(0, 11)
   if (beginning !== '"use babel"' && beginning !== "'use babel'") {
     return contents
@@ -13,7 +14,7 @@ export function process(contents, {fileName, relativePath, root}, {config, state
   const transpiled = transform(contents, Object.assign({}, config.babel, {
     filename: fileName,
     filenameRelative: relativePath,
-    sourceRoot: root,
+    sourceRoot: Path.join('sources', Path.dirname(relativePath)),
     sourceMaps: true,
     highlightCode: false
   }))
