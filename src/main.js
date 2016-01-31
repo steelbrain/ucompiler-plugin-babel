6296624a9f5e95aeec68e6c7afe6b1c70496773a
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-31 16:55:46 -0700

    :arrow_up: Upgrade package to latest ucompiler API

--- src/main.js
+++ src/main.js
@@ -5,19 +5,23 @@ import Path from 'path'
 
 export const compiler = true
 export const minifier = false
-export function process(contents, {fileName, relativePath}, {config, state}) {
+export function process(contents, {rootDirectory, filePath, config}) {
   const beginning = contents.substr(0, 11)
   if (beginning !== '"use babel"' && beginning !== "'use babel'") {
-    return contents
+    return null
   }
 
+  const fileName = Path.dirname(filePath)
+  const relativePath = Path.relative(rootDirectory, filePath)
   const transpiled = transform(contents, Object.assign({}, config.babel, {
     filename: fileName,
     filenameRelative: relativePath,
-    sourceRoot: Path.join('sources', Path.dirname(relativePath)),
+    sourceRoot: rootDirectory,
     sourceMaps: true,
     highlightCode: false
   }))
-  state.sourceMap = transpiled.map
-  return transpiled.code
+  return {
+    contents: transpiled.code,
+    sourceMap: transpiled.map
+  }
 }
