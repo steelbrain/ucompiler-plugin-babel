f73eb6af5d1b32dcaf53ca26fa2c99c7edd89514
steelbrain <aneesiqbalbhatti@gmail.com>
2016-02-03 08:26:49 -0700

    :fire: Remove beginning check for 'use babel'

--- src/main.js
+++ src/main.js
@@ -6,11 +6,6 @@ import Path from 'path'
 export const compiler = true
 export const minifier = false
 export function process(contents, {rootDirectory, filePath, config}) {
-  const beginning = contents.substr(0, 11)
-  if (beginning !== '"use babel"' && beginning !== "'use babel'") {
-    return null
-  }
-
   const fileName = Path.dirname(filePath)
   const relativePath = Path.relative(rootDirectory, filePath)
   const transpiled = transform(contents, Object.assign({}, config.babel, {
